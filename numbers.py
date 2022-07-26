f5d1c7497bf0f207a5625450882cd5052027b985
Balamurugan84 <102845708+Balamurugan84@users.noreply.github.com>
2022-07-26 09:56:33 +0530

    Create numbers.py

---
+++ numbers.py
@@ -0,0 +1,24 @@
+x = 1
+y = 356562225
+z = -3255522
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+x = 5.10
+y = 3.045
+z = -74.26
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = 35e3
+y = 12E4
+z = -87.7e100
+
+print(type(x))
+print(type(y))
+print(type(z))
