ba8e93771e64004ac694876ffe4588eff2f691ab
Balamurugan84 <102845708+Balamurugan84@users.noreply.github.com>
2022-07-26 10:11:23 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,13 @@
+list = ["chennai", "andra", "kerala"]
+print(list)
+
+
+list2 = ["chennai", "andra", "kerala", "andra", "kerala"]
+print(list2)
+
+list3 = ["chennai", "andra", "kerala"]
+print(len(list3))
+
+
+list4 = ["chennai", "andra", "kerala"]
+print(type(list4))
