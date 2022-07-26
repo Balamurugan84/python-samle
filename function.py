443c74669f00427978f83aab42e7a40a08f56f91
Balamurugan84 <102845708+Balamurugan84@users.noreply.github.com>
2022-07-26 10:00:44 +0530

    Create function.py

---
+++ function.py
@@ -0,0 +1,22 @@
+def function():
+  print("Hello World")
+
+function()
+
+
+
+def function2(child3, child2, child1):
+  print("The youngest child is " + child3)
+
+function2(child1 = "raj", child2 = "vijay", child3 = "bala")
+
+
+
+
+def function3(foods):
+  for x in foods:
+    print(x)
+
+fruits = ["apple", "banana", "orange"]
+
+function3(fruits)
